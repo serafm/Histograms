f50d88225cc63b9c399e66ce8c541f179450e4ae
serafm <cs04555@uoi.gr>
2023-03-16 14:29:17 +0200

    Added comments

--- histograms.py
+++ histograms.py
@@ -2,18 +2,19 @@ import csv
 
 global data, incomeList, bins
 
-# List of Income field
+# List of Income attribute
 incomeList = list()
 
 bins = dict()
 
 
-# Read csv file data and add them into a list
+# Read csv file Income data, add them into a list
 def read_data():
     global data
     with open('acs2015_census_tract_data.csv', newline='') as csvfile:
         data = list(csv.reader(csvfile))
         for income in range(1, len(data)):
+            # if the field is empty don't add it in list
             if data[income][13] == '':
                 continue
             else:
@@ -22,35 +23,42 @@ def read_data():
 
 read_data()
 incomeList = sorted(incomeList)
+# Minimum income
 min_income = min(incomeList)
+# Maximum income
 max_income = max(incomeList)
 
 
+# Create an equi-width histogram
 def equiwidth():
     global incomeList, bins
     start = 0
     min = min_income
-    width = (max_income - min_income)/100
+    # The width for exact distribution of the values into the bins
+    width = (max_income - min_income) / 100
     for i in range(100):
-        temp = [round(min, 2), round(min+width, 2)]
+        # Range of every bin
+        temp = [round(min, 2), round(min + width, 2)]
         b = str(temp).replace(']', ')')
+        # Initialize the dictionary
         bins[b] = []
+        # Add income values into the bins
         for income in range(start, len(incomeList)):
-            if min <= incomeList[income] < min+width:
+            if min <= incomeList[income] < min + width:
                 bins[b].append(incomeList[income])
             else:
                 start = income
                 break
+        # Update the value of min
         min = round(min + width, 2)
 
 
-#def equidepth():
+# def equidepth():
 
 
-
-print(str(len(incomeList)) + ' valid income values')
-print("minimum income = " + str(min_income))
-print("maximum income = " + str(max_income))
+print(len(incomeList), ' valid income values')
+print("minimum income = ", min_income)
+print("maximum income = ", max_income)
 print("equiwidth:")
 equiwidth()
 for key in bins:
